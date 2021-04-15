a41da46b65882f3e0e22c13969ecc0d755a1f1c7
brunamello <brunacrespomello@gmail.com>
2021-04-15 22:16:10 +0100

    01. Primeiro Repositorio
    Nessa aula aprendemos:
    - qual a stack necessária para nosso projeto;
    configurar nosso projeto Spring para conectar ao banco de dados;
    - criar tabelas de forma automática e alterar o nome delas conforme a necessidade da aplicação;
    - utilizar os repositórios do framework para trazer agilidade nas nossas operações de CRUD.

--- src/main/java/br/com/alura/spring/data/SpringDataJpaRepositoriosConsultasProjecoesSpecificationsApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataJpaRepositoriosConsultasProjecoesSpecificationsApplication.java
@@ -1,13 +1,29 @@
 package br.com.alura.spring.data;
 
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataJpaRepositoriosConsultasProjecoesSpecificationsApplication {
+public class SpringDataJpaRepositoriosConsultasProjecoesSpecificationsApplication implements CommandLineRunner {
+
+    private final CargoRepository repository;
+
+    public SpringDataJpaRepositoriosConsultasProjecoesSpecificationsApplication(CargoRepository repository) {
+        this.repository = repository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaRepositoriosConsultasProjecoesSpecificationsApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cargo cargo = new Cargo();
+        cargo.setDescricao("Desenvolvedor de Software");
+        repository.save(cargo);
+    }
 }
